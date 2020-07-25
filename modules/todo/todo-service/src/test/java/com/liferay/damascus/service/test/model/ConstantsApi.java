59769e6b3d4b42ebfdf142ee6817f09f7db3bbd2
Diego Furtado <diegotomfurtado@gmail.com>
2020-07-25 20:10:17 -0300

    Rename package name.

---
+++ modules/todo/todo-service/src/test/java/com/liferay/damascus/service/test/model/ConstantsApi.java
@@ -0,0 +1,11 @@
+package com.liferay.damascus.service.test.model;
+
+public class ConstantsApi {
+
+	public static final String API_PATH = "todo.todo";
+	public static final String DELETE = "delete-entry";
+	public static final String ADD_NEW_TODO_LIST = "add-entry";
+	
+	public static final String EMAIL = "test@liferay.com";
+	public static final String PASSWORD = "test";
+}
---
+++ modules/todo/todo-service/src/test/java/com/liferay/damascus/service/test/model/ToDo.java
@@ -0,0 +1,62 @@
+package com.liferay.damascus.service.test.model;
+
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ToDo {
+
+	private String title;
+
+	private String todoRichText;
+	private String todoSummaryName;
+	private String urlTitle;
+	private String todoText;
+	private String todoTitleName;
+	private Boolean todoBooleanStat;
+
+	@JsonIgnore
+	private Long groupId;
+	@JsonIgnore
+	private Long companyId;
+	@JsonIgnore
+	private Integer mvccVersion;
+	@JsonIgnore
+	private Integer status;
+	@JsonIgnore
+	private Integer statusByUserId;
+	@JsonIgnore
+	private String statusByUserName;
+	@JsonIgnore
+	private Instant createDate;
+	@JsonIgnore
+	private Instant modifiedDate;
+	@JsonIgnore
+	private Instant statusDate;
+	@JsonIgnore
+	private Instant todoDateTime;
+	@JsonIgnore
+	private String todoDocumentLibrary;
+	@JsonIgnore
+	private Double todoDouble;
+	@JsonIgnore
+	private Integer todoId;
+	@JsonIgnore
+	private Integer todoInteger;
+	@JsonIgnore
+	private Integer userId;
+	@JsonIgnore
+	private String userName;
+	@JsonIgnore
+	private String uuid;
+
+}
